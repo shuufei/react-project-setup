fd069a4c90c313c4a85a014788e3c7c8be44fa3c
shuufei <digital.shuufei@gmail.com>
2020-04-29 16:41:11 +0900

    prettier導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["airbnb-typescript"],
+  extends: [
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
   parserOptions: {
     project: "./tsconfig.json",
   },
